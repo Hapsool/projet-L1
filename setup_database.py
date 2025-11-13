7ff2d180da8fe7e78de8354b909431ce0e0ee430
Nicolas Georges <georges1.colas@gmail.com>
2025-11-13 22:01:13 +0100

    Update setup_database.py

--- setup_database.py
+++ setup_database.py
@@ -2,8 +2,10 @@ import sqlite3
 from config import DB_PATH
 
 def setup():
-  connect = sqlite3.connect(DB_PATH)
-  connect.execute("PRAGMA journal_mode=WAL;")
+  conn = sqlite3.connect(DB_PATH)
+  conn.execute("PRAGMA journal_mode=WAL;")
+
+  # Créations des tables si elles n'existes pas :
   connect.executescript("""
   CREATE TABLE IF NOT EXISTS mesures (
     pir INTEGER,
@@ -17,9 +19,9 @@ def setup():
   );
   CREATE TABLE IF NOT EXISTS couleurs (
     id INTEGER PRIMARY KEY,
-    R INTEGER NOT NULL,
-    G INTEGER NOT NULL,
-    B INTEGER NOT NULL
+    r INTEGER NOT NULL,
+    g INTEGER NOT NULL,
+    b INTEGER NOT NULL
   );
   CREATE TABLE IF NOT EXISTS images (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -28,8 +30,31 @@ def setup():
     matrice TEXT NOT NULL
   );
   """)
-  connect.commit()
-  connect.close()
+
+  # Mise en place des valeurs de départ si les tables sont vides :
+
+  # pour la table mesures :
+  resultat = conn.execute("SELECT COUNT(*) FROM mesures")
+  nb = resultat.fetchone()[0]
+  if nb_mesures == 0 :
+    conn.execute("INSERT INTO mesures (pir, light, sound) VALUES (?, ?, ?)", (0,0,0))
+
+  # pour la table config :
+  resultat = conn.execute("SELECT COUNT(*) FROM config")
+  nb = resultat.fetchone()[0]
+  if nb_mesures == 0 :
+    conn.execute("INSERT INTO config (mode, luminosite, jeu_de_lumiere) VALUES (?, ?, ?)", ("manual",50,"static"))
+
+  # pour la table couleurs :
+  resultat = conn.execute("SELECT COUNT(*) FROM couleurs")
+  nb = resultat.fetchone()[0]
+  if nb_mesures == 0 :
+    conn.executemany("INSERT INTO couleurs (id, r, g, b) VALUES (?, ?, ?, ?)", [(i,255,255,255) for i in range(1,65)])
+
+  # La table images peut être vide.
+    
+  conn.commit()
+  conn.close()
 
 if __name__ == "__main__":
   setup()
