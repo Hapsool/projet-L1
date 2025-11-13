5d105043d43c288fbc77ac0411ec9959058d4b33
Nicolas Georges <georges1.colas@gmail.com>
2025-11-13 21:33:32 +0100

    Update setup_database.py

--- setup_database.py
+++ setup_database.py
@@ -1,15 +1,35 @@
 import sqlite3
 from config import DB_PATH
 
-def init_db(path, table_sql):
-  connect = sqlite3.connect(path)
+def setup():
+  connect = sqlite3.connect(DB_PATH)
   connect.execute("PRAGMA journal_mode=WAL;")
-  connect.execute(table_sql)
+  connect.executescript("""
+  CREATE TABLE IF NOT EXISTS mesures (
+    pir INTEGER,
+    light INTEGER,
+    sound INTEGER 
+  );
+  CREATE TABLE IF NOT EXISTS config (
+    mode TEXT NOT NULL,
+    luminosite INTEGER NOT NULL,
+    jeu_de_lumiere TEXT NOT NULL
+  );
+  CREATE TABLE IF NOT EXISTS couleurs (
+    id INTEGER PRIMARY KEY,
+    R INTEGER NOT NULL,
+    G INTEGER NOT NULL,
+    B INTEGER NOT NULL
+  );
+  CREATE TABLE IF NOT EXISTS images (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nom TEXT NOT NULL,
+    date_creation TEXT,
+    matrice TEXT NOT NULL
+  );
+  """)
   connect.commit()
   connect.close()
-  
-def setup():
-  pass
 
 if __name__ == "__main__":
   setup()
