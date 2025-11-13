c4990ef59808512e032d854171ea7c0609d14711
Nicolas Georges <georges1.colas@gmail.com>
2025-11-13 19:32:00 +0100

    Update setup_database.py

--- setup_database.py
+++ setup_database.py
@@ -1,2 +1,15 @@
 import sqlite3
 from config import DB_CAPTEURS, DB_SITE
+
+def init_db(path, table_sql):
+  connect = sqlite3.connect(path)
+  connect.execute("PRAGMA journal_mode=WAL;")
+  connect.execute(table_sql)
+  connect.commit()
+  connect.close()
+  
+def setup():
+  pass
+
+if __name__ == "__main__":
+  setup()
