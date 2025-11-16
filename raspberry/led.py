61e2b4de00db54d978f7fa32e7e8e73cc9a92a56
Nicolas Georges <georges1.colas@gmail.com>
2025-11-16 17:18:22 +0100

    Update led.py

--- raspberry/led.py
+++ raspberry/led.py
@@ -1,4 +1,18 @@
+import board
+import neopixel
 from config import DB_PATH
+
+LED_PIN = board.D18
+NUM_LEDS = 64
+
+pixels = neopixel.NeoPixel(
+    LED_PIN,
+    NUM_LEDS,
+    brightness=0.3,      # intensité globale (0.0 à 1.0)
+    auto_write=False,     # écriture automatique
+    pixel_order=neopixel.GRB  # l'ordre le plus courant
+)
+
 
 def eteindre_led():
   pass
