c09e520395a8dba174b4255d9eccddeac9be584b
Nicolas Georges <georges1.colas@gmail.com>
2025-11-19 14:20:16 +0100

    Update api.py

--- web/api.py
+++ web/api.py
@@ -1,1 +1,17 @@
+import uvicorn
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
+app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates/")
+
+
+
+
+
+
+if __name__ == "__main__":
+  uvicorn.run(app)
