cbecd30e636c6197ad2ecf828f8368e42369994c
Nicolas Georges <georges1.colas@gmail.com>
2025-11-20 20:04:18 +0100

    Update api.py

--- web/api.py
+++ web/api.py
@@ -11,21 +11,32 @@ app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates/")
 
-@app.post("/controle")
-async def controle_led(request: Request):
-  data =  await request.form()
-  luminosite = float(data.get("luminosite"))
-  etat = int(data.get("etat"))
-  
-  conn = sqlite3.connect(DB_PATH)
-  conn.execute("UPDATE config SET luminosite = ?",(luminosite,))
-  conn.execute("UPDATE config SET etat = ?",(etat,))
-  conn.commit()
-  conn.close()
-  
-  return {"luminosite":luminosite,"etat":etat}
+@app.post("/led/state")
+async def update_led_state(request: Request):
+    data = await request.form()
+    etat = int(data.get("etat"))
+
+    conn = sqlite3.connect(DB_PATH)
+    conn.execute("UPDATE config SET etat = ?", (etat,))
+    conn.commit()
+    conn.close()
+
+    return {"etat": etat}
+
+@app.post("/led/luminosity")
+async def update_led_luminosity(request: Request):
+    data = await request.form()
+    luminosite = float(data.get("luminosite"))
+
+    conn = sqlite3.connect(DB_PATH)
+    conn.execute("UPDATE config SET luminosite = ?", (luminosite,))
+    conn.commit()
+    conn.close()
+
+    return {"luminosite": luminosite}
+
 
 
 if __name__ == "__main__":
-  from config import PORT
-  uvicorn.run(app, host="0.0.0.0", port=PORT)
+  from config import API_PORT
+  uvicorn.run(app, host="0.0.0.0", port=API_PORT)
