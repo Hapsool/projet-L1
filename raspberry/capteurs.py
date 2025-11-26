b7919a699a6ae2509dc63effa14af6064a0b6e60
Nicolas Georges <georges1.colas@gmail.com>
2025-11-26 13:26:41 +0100

    Update capteurs.py

--- raspberry/capteurs.py
+++ raspberry/capteurs.py
@@ -1,3 +1,18 @@
 import RPi.GPIO as GPIO
 from config import DB_PATH, LIGHT_SENSOR_PIN, SOUND_SENSOR_PIN, MOTION_SENSOR_PIN
 GPIO.setmode(GPIO.BCM)
+GPIO.setup(LIGHT_SENSOR_PIN, GPIO.IN)
+GPIO.setup(SOUND_SENSOR_PIN, GPIO.IN)
+GPIO.setup(MOTION_SENSOR_PIN, GPIO.IN)
+
+def read_light(): return GPIO.input(LIGHT_SENSOR_PIN)
+def read_sound(): return GPIO.input(SOUND_SENSOR_PIN)
+def read_motion(): return GPIO.input(MOTION_SENSOR_PIN)
+def read_all(): return {"light":read_light(),"sound":read_sound(),"pir":read_motion()}
+def cleanup(): GPIO.cleanup()
+
+if __name__ == "__main__" :
+  import time
+  while True :
+    print(read_all())
+    time.sleep(1)
