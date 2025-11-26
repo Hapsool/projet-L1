7a9ff27c4be62c984f4ffeb75341115de2ce070c
Nicolas Georges <georges1.colas@gmail.com>
2025-11-26 14:03:24 +0100

    e

--- raspberry/run_pi.py
+++ raspberry/run_pi.py
@@ -1,9 +1,20 @@
 import asyncio
-import 
+import uvicorn
+import sqlite3
+from config import DB_PATH,API_PORT
+from web.api import app
+from capteurs import read_all, cleanup
+from led import allumer_led, eteindre_led, changer_luminosite
 
+async def boucle_led():
+  while True :
+    
+    await asyncio.sleep(0.1)
 
 async def main():
-  server = 
+  asyncio.create_task(boucle_led())
+  config = uvicorn.Config(app=app, host="0.0.0.0", port=API_PORT)
+  server = uvicorn.Server(config)
   await server.serve()
 
 if __name__ == "__main__":
