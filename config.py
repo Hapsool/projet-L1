ccf787f5017387e8e691afda671c0d8a803a2ff7
Nicolas Georges <georges1.colas@gmail.com>
2025-11-28 23:35:01 +0100

    changer SOUND_SENSOR_PIN à 1

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ DB_PATH = "data/data_base.db" # Chemin vers la base de données
 API_PORT = 8000 #
 NUM_LEDS = 64 # Le nombre de leds dans la matrice (8x8)
 LIGHT_SENSOR_PIN = 0 # Numéro de pin pour le light sensor
-SOUND_SENSOR_PIN = 5 # Numéro de pin pour le sound sensor
+SOUND_SENSOR_PIN = 1 # Numéro de pin pour le sound sensor
 MOTION_SENSOR_PIN = 16 # Numéro de pin pour le motion sensor
