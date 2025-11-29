04dc680fdeae40a82e7ef255be4b5d023e657704
Hapsool <raphaelrobin93@gmail.com>
2025-11-29 02:04:57 +0100

    ajout de valeurs par défauts
    Added default threshold values for audio and light sensors, and default LED intensity.

--- config.py
+++ config.py
@@ -4,3 +4,7 @@ NUM_LEDS = 64 # Le nombre de leds dans la matrice (8x8)
 LIGHT_SENSOR_PIN = 0 # Numéro de pin pour le light sensor
 SOUND_SENSOR_PIN = 1 # Numéro de pin pour le sound sensor
 MOTION_SENSOR_PIN = 16 # Numéro de pin pour le motion sensor
+
+CAPTEUR_AUDIO_SEUIL_DEFAUT = 50 #la valeur par défaut de la sensibilité du capteur audio
+CAPTEUR_LUMIERE_SEUIL_DEFAUT = 100 #la valeur par défaut de la sensibilité du capteur de lumiere
+INTENSITE_LUMIERE_DEFAUT = 30 #la valeur par défaut de l'intensité lumineuse des leds
