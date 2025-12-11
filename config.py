2b7077c54ae96087d095f1ce096783f47cf73642
Nicolas Georges <georges1.colas@gmail.com>
2025-12-11 18:20:24 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 DB_PATH = "data/data_base.db" # Chemin vers la base de données
 API_PORT = 8000 #
-NUM_LEDS = 64 # Le nombre de leds dans la matrice (8x8)
+NUM_LEDS = 12 # Le nombre de leds dans la matrice (8x8)
 LIGHT_SENSOR_PIN = 0 # Numéro de pin pour le light sensor
 SOUND_SENSOR_PIN = 2 # Numéro de pin pour le sound sensor
 MOTION_SENSOR_PIN = 16 # Numéro de pin pour le motion sensor
