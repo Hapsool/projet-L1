d6840b8295a1cc5bef635242e2af68e54ff57404
Nicolas Georges <georges1.colas@gmail.com>
2025-12-16 22:34:59 +0100

    Update run_pi.py

--- raspberry/run_pi.py
+++ raspberry/run_pi.py
@@ -1,12 +1,21 @@
 import asyncio
 import uvicorn
 import sqlite3
-from config import DB_PATH,API_PORT
+from config import DB_PATH,API_PORT,NUM_LEDS
 from web.api import app
 from raspberry.capteurs import read_light, read_sound, read_motion, read_all, cleanup
 from raspberry.led import allumer_led, eteindre_led, changer_luminosite, toggle_led
 
 animation_task = None
+
+last_state = {
+  "etat":None,
+  "luminosite":None,
+  "jeu":None,
+  "animation":None,
+  "couleur":None,
+  "image":None
+}
 
 async def boucle_capteurs():
     while True:
@@ -19,75 +28,135 @@ async def boucle_capteurs():
       await asyncio.sleep(0.5)
 
 async def strobe():
-  while True :
-    allumer_led((255,0,0))
-    for i in range(1,256):
-      allumer_led((255-i,i,0))
-      asyncio.sleep(0.05)
-
-    allumer_led((0,255,0))
-    for i in range(1,256):
-      allumer_led((0,255-i,i))
-      asyncio.sleep(0.05)
-
-    allumer_led((0,0,255))
-    for i in range(1,256):
-      allumer_led((i,0,255-i))
-      asyncio.sleep(0.05)
+  try :
+    while True:
+      allumer_led((255,0,0))
+      for i in range(1,256):
+        allumer_led((255-i,i,0))
+        await asyncio.sleep(0.035)
+      allumer_led((0,255,0))
+      for i in range(1,256):
+        allumer_led((0,255-i,i))
+        await asyncio.sleep(0.035)
+      allumer_led((0,0,255))
+      for i in range(1,256):
+        allumer_led((i,0,255-i))
+        await asyncio.sleep(0.035)
+  except asyncio.CancelledError :
+    eteindre_led()
+    raise
 
 async def fade():
-  while True :
+  try :
+    while True:
+      allumer_led((255,0,0))
+      await asyncio.sleep(1)
+      for i in range(1,256) :
+        allumer_led((255,i,i))
+        await asyncio.sleep(0.005)
+      allumer_led((0,255,0))
+      await asyncio.sleep(1)
+      for i in range(1,256) :
+        allumer_led((i,255,i))
+        await asyncio.sleep(0.005)
+      allumer_led((0,0,255))
+      await asyncio.sleep(1)
+      for i in range(1,256) :
+        allumer_led((i,i,255))
+        await asyncio.sleep(0.005)
+  except asyncio.CancelledError :
+    eteindre_led()
+    raise
 
-    allumer_led((255,0,0))
-    asyncio.sleep(1)
-    for i in range(1,256) :
-      allumer_led((255,i,i))
-      asyncio.sleep(0.05)
-
-    allumer_led((0,255,0))
-    asyncio.sleep(1)
-    for i in range(1,256) :
-      allumer_led((i,255,i))
-      asyncio.sleep(0.05)
-
-    allumer_led((0,0,255))
-    asyncio.sleep(1)
-    for i in range(1,256) :
-      allumer_led((i,i,255))
-      asyncio.sleep(0.05)
 
 async def flash():
-  while True :
-    allumer_led((255,0,0))
-    asyncio.sleep(2)
-    allumer_led((0,255,0))
-    asyncio.sleep(2)
-    allumer_led((0,0,255))
-    asyncio.sleep(2)
+  try :
+    while True:
+      allumer_led((255,0,0))
+      await asyncio.sleep(1.5)
+      allumer_led((0,255,0))
+      await asyncio.sleep(1.5)
+      allumer_led((0,0,255))
+      await asyncio.sleep(1.5)
+  except asyncio.CancelledError :
+    eteindre_led()
+    raise
 
-def choix_lumiere(jeu_de_lumiere,couleur_actif,image_actif,animation_actif) : 
-    global animation_task
-    if jeu_de_lumiere == "couleur":
-        allumer_led(couleur_actif)
-    elif jeu_de_lumiere == "image":
-        allumer_led(image_actif)
-    elif jeu_de_lumiere == "animation":
+def wheel(pos):
+    if pos < 85:
+        return (pos * 3, 255 - pos * 3, 0)
+    elif pos < 170:
+        pos -= 85
+        return (255 - pos * 3, 0, pos * 3)
+    else:
+        pos -= 170
+        return (0, pos * 3, 255 - pos * 3)
+
+async def rainbow():
+  try :
+    while True:
+        for j in range(255):
+          l = []
+          for i in range(NUM_LEDS):
+            rc_index = (i * 256 // NUM_LEDS) + j
+            l.append(wheel(rc_index & 255))
+          allumer_led(l)
+          await asyncio.sleep(0.01)
+  except asyncio.CancelledError :
+    eteindre_led()
+    raise
+
+def stop_animation():
+  global animation_task
+  if animation_task is not None and not animation_task.done():
+    animation_task.cancel()
+    animation_task = None
+    
+def start_animation(anim):
+  global animation_task
+  stop_animation()
+  animation_task = asyncio.create_task(anim)
+  
+def choix_lumiere(etat,luminosite,jeu_de_lumiere,couleur_actif,image_actif,animation_actif) : 
+  global animation_task,last_state
+  
+  current_state = {
+    "etat":etat,
+    "luminosite":luminosite,
+    "jeu":jeu_de_lumiere,
+    "animation":animation_actif,
+    "couleur":couleur_actif,
+    "image":image_actif
+  }
+
+  if etat == 0 :
+    eteindre_led()
+  else :
+    changer_luminosite(luminosite)
+    if jeu_de_lumiere == "animation":
+      if (animation_actif != last_state["animation"]) or (jeu_de_lumiere != last_state["jeu"]):
         if animation_actif == "strobe":
-            if animation_task is None or animation_task.done():
-                animation_task = asyncio.create_task(strobe())
+          start_animation(strobe())
         elif animation_actif == "fade":
-            if animation_task is None or animation_task.done():
-                animation_task = asyncio.create_task(fade())
+          start_animation(fade())
+        elif animation_actif == "flash":
+          start_animation(flash())
+        elif animation_actif == "rainbow":
+          start_animation(rainbow())
         else:
-            if animation_task is not None and not animation_task.done():
-                animation_task.cancel()
-                animation_task = None
+          stop_animation()
+    else :
+      stop_animation()
+      if jeu_de_lumiere == "couleur":
+          allumer_led(couleur_actif)
+      elif jeu_de_lumiere == "image":
+          allumer_led(image_actif)
+    last_state = current_state
 
 async def boucle_led():
   while True :
     conn = sqlite3.connect(DB_PATH)
     config = conn.execute("SELECT * FROM config").fetchone()
-
     mode = config[0]
     etat = config[1]
     luminosite = config[2]
@@ -97,43 +166,23 @@ async def boucle_led():
     couleur_actif	= eval(config[6])
     image_actif	= config[7]
     animation_actif = config[8]
-
     conn.close()
-
-    changer_luminosite(luminosite)
-
     if mode == "manual" :
-      if etat == 0 :
-        eteindre_led()
-      elif etat == 1 :
-        choix_lumiere(jeu_de_lumiere,couleur_actif,image_actif,animation_actif)
-
+      choix_lumiere(etat,luminosite,jeu_de_lumiere,couleur_actif,image_actif,animation_actif)
     elif mode == "auto" :
-      etat_led = 0
-      if read_light() >= lum_min :
-        eteindre_led()
-      else :
-        if read_motion() == 0 :
-          eteindre_led()
-        elif read_motion() == 1 :
-          choix_lumiere(jeu_de_lumiere,couleur_actif,image_actif,animation_actif)
-          etat_led = 1
+      etat_led = 0 if read_light() >= lum_min else read_motion()
+      choix_lumiere(etat_led,luminosite,jeu_de_lumiere,couleur_actif,image_actif,animation_actif)
       conn = sqlite3.connect(DB_PATH)
       conn.execute("UPDATE config SET etat = ?",(etat_led,))
       conn.commit()
       conn.close()
-
     elif mode == "sound":
-      if read_sound() >= audio_min :
-        if etat == 0 :
-          choix_lumiere(jeu_de_lumiere,couleur_actif,image_actif,animation_actif)
-        elif etat == 1 :
-          eteindre_led()
-        conn = sqlite3.connect(DB_PATH)
-        conn.execute("UPDATE config SET etat = ?",((1-etat),))
-        conn.commit()
-        conn.close()
-  
+      etat_led = 1-etat if read_sound() >= audio_min else etat
+      choix_lumiere(etat_led,luminosite,jeu_de_lumiere,couleur_actif,image_actif,animation_actif)
+      conn = sqlite3.connect(DB_PATH)
+      conn.execute("UPDATE config SET etat = ?",(etat_led,))
+      conn.commit()
+      conn.close()
     await asyncio.sleep(0.1)
 
 @app.on_event("startup")
