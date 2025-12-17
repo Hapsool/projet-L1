7753264c476c973bf561896314a2cf9c22f4f9e3
Nicolas Georges <georges1.colas@gmail.com>
2025-12-17 11:43:17 +0100

    Delete essai directory

--- essai/raspberry/led.py
+++
@@ -1,9 +0,0 @@
-import time, board, neopixel
-from config import NUM_LEDS
-LED_PIN = board.D18
-pixels = neopixel.NeoPixel(LED_PIN, NUM_LEDS, brightness=0.3, auto_write=False, pixel_order=neopixel.GRB)
-def eteindre_led(): pixels.fill((0,0,0)); pixels.show()
-def allumer_led(c): 
-    for i in range(NUM_LEDS): pixels[i]=c[i] if i<len(c) else (0,0,0)
-    pixels.show()
-def changer_luminosite(v): pixels.brightness=max(0,min(100,v))/100; pixels.show()
